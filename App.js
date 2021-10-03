64a2df221688b88c4a404a68cbbaf0b2b14762f3
TomKim <babooteng2@gmail.com>
2021-10-04 00:04:44 +0900

    #1.5 Styles

--- App.js
+++ App.js
@@ -5,17 +5,47 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark"></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>        
+      </View>      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1, backgroundColor:"tomato"
   },
-});
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",    
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "900"
+  },
+  weather: {
+    flex: 3,        
+  },
+  day: {
+    flex: 1,
+    backgroundColor: "teal",    
+    alignItems: "center",    
+  },
+  temp: {
+    margin: 30,
+    fontSize: 108
+  },
+  desc: {
+    margin: -40,
+    fontSize: 40
+  }
+})
